b6df235afdcbdbbcf5b81170ae9418f8cfd93ec0
Wayne <dev.wayne.ix@gmail.com>
2014-09-10 20:22:41 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var cssmin = require("gulp-cssmin");
+//var jade = require('gulp-jade');
 
 gulp.task("less", function(){
     gulp.src("app/less/*.less")
@@ -12,5 +13,15 @@ gulp.task("less", function(){
 gulp.task('watch', function () {
     gulp.watch('app/less/*.less', ['less']);
 });
+
+/*gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+
+  gulp.src('./app/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./dist/'))
+});*/
 
 gulp.task("default", ["less"]);
